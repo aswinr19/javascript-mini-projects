6309f2d4b25b3ffda07299a009097110d29158db
aswinr <aswinr2442002@gmail.com>
2022-07-29 20:12:47 +0530

    added functionality to save to tabs

--- lead-tracker-chrome-extension/index.js
+++ lead-tracker-chrome-extension/index.js
@@ -21,6 +21,11 @@ inputBtn.addEventListener("click",function(){
 
 saveTabBtn.addEventListener("click",function(){
 	
+	chrome.tabs.query({active:true , currentWindow: true}, function(tabs){
+		myLeads.push(tabs[0].url)
+		localStorage.setItem("myLeads", JSON.stringify( myLeads ) )
+		render(myLeads)
+	})
 })
 
 deleteBtn.addEventListener("click",function(){
