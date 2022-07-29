1cc6932084354e14e2d598ab25ce756eb524a130
aswinr <aswinr2442002@gmail.com>
2022-07-29 19:51:08 +0530

    added functionality to save to local storage

--- lead-tracker-chrome-extension/index.js
+++ lead-tracker-chrome-extension/index.js
@@ -1,20 +1,33 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const saveTabBtn = document.getElementById("save-tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl= document.getElementById("ul-el")
 
+const leadsFromLocalStorage =  JSON.parse( localStorage.getItems("myLeads") )
+
+if(leadsFromLocalStorage){
+	myLeads = leadsFromLocalStorage
+	render(myLeads)
+}
+
 inputBtn.addEventListener("click",function(){
+	
+	localStorage.setItem = JSON.stringify(myLeads)
 	myLeads.push(inputEl.value)
 	inputEl.value = ""
-	renderLeads()
-}
-)	
+	render(myLeads)
+})	
 
+saveTabBtn.addEventListener("click",function(){
+	
+})
 
-function renderLeads(){
+function render(leads){
 	let listItems = ""
-	for(let i = 0; i < myLeads.length; i++){
-		listItems += `<li> <a href="${myLeads[i]}" > ${myLeads[i]} </a> </li>`	
+	for(let i = 0; i < leads.length; i++){
+		listItems += `<li> <a href="${leads[i]}" > ${leads[i]} </a> </li>`	
 	}
 	ulEl.innerHTML = listItems
 }
