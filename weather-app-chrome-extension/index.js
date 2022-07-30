dee4698a763f2a2d9b264bb69e7764531e8552bb
aswinr <aswinr2442002@gmail.com>
2022-07-30 13:19:13 +0530

    base setup

---
+++ weather-app-chrome-extension/index.js
@@ -0,0 +1,10 @@
+console.log("hello world!")
+
+
+// fetch('http://example.com/movies.json')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
