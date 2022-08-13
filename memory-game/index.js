e244443ff1482616de161eccf2ab8113177488f0
aswinr <aswinr2442002@gmail.com>
2022-08-13 20:26:11 +0530

    added new pokemon

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,4 +1,4 @@
-const pokemon = [
+const data = [
   {
     name: "Articuno",
     img: "images/articuno.png",
@@ -39,48 +39,88 @@ const pokemon = [
     name: "Herdier",
     img: "images/herdier.png",
   },
-
   {
-    name: "Articuno",
-    img: "images/articuno.png",
+    name: "Kecleon",
+    img: "images/kecleon.png",
   },
   {
-    name: "Bulbasaur",
-    img: "images/bulbasaur.png",
+    name: "Krokorok",
+    img: "images/krokorok.png",
   },
   {
-    name: "Butterfree",
-    img: "images/butterfree.png",
+    name: "Lapras",
+    img: "images/lapras.png",
   },
   {
-    name: "Chansey",
-    img: "images/chansey.png",
+    name: "Meowth",
+    img: "images/meowth.png",
   },
   {
-    name: "Charizard",
-    img: "images/charizard.png",
+    name: "Minun",
+    img: "images/minun.png",
   },
   {
-    name: "Dragonair",
-    img: "images/dragonair.png",
+    name: "Nidoqueen",
+    img: "images/nidoqueen.png",
   },
   {
-    name: "Dragonite",
-    img: "images/dragonite.png",
+    name: "Octillery",
+    img: "images/octillery.png",
   },
   {
-    name: "Flareon",
-    img: "images/flareon.png",
+    name: "Persian",
+    img: "images/persian.png",
   },
   {
-    name: "Fletchling",
-    img: "images/fletchling.png",
+    name: "Phanpy",
+    img: "images/phanpy.png",
   },
   {
-    name: "Herdier",
-    img: "images/herdier.png",
+    name: "Piplup",
+    img: "images/piplup.png",
+  },
+  {
+    name: "Popplio",
+    img: "images/popplio.png",
+  },
+  {
+    name: "Pyroar",
+    img: "images/pyroar.png",
+  },
+  {
+    name: "Sandshrew",
+    img: "images/sandshrew.png",
+  },
+  {
+    name: "Shinx",
+    img: "images/shinx.png",
+  },
+  {
+    name: "Sneasel",
+    img: "images/sneasel.png",
+  },
+  {
+    name: "Solgaleo",
+    img: "images/solgaleo.png",
+  },
+  {
+    name: "Zapdos",
+    img: "images/zapdos.png",
   },
 ];
+
+data.sort(() => 0.5 - Math.random());
+
+// console.log(data);
+
+let pokemon = [];
+
+for (let i = 0; i < 10; i++) {
+  pokemon.push(data[i]);
+  pokemon.push(data[i]);
+}
+
+// console.log(pokemon);
 
 const grid = document.getElementById("grid");
 const score = document.getElementById("score");
@@ -92,11 +132,12 @@ let selectedPokeball = [];
 
 let selectedPokeballId = [];
 
-let openedPokeball = []
+let openedPokeball = [];
 
 pokemon.sort(() => 0.5 - Math.random());
 
-//console.log(pokemon)
+pokemon.sort(() => 0.5 - Math.random());
+// console.log(pokemon)
 
 function createBoard() {
   for (let i = 0; i < pokemon.length; i++) {
@@ -110,38 +151,37 @@ function createBoard() {
 
 createBoard();
 
-function renderScore(){
+function renderScore() {
+  score.textContent = openedPokeball.length;
 
-	score.textContent = openedPokeball.length;
-
-	if(openedPokeball.length === 10)
-	{
-		alert("You won!");
-
-	}
+  if (openedPokeball.length === 10) {
+    alert("You won!");
+  }
 }
-function checkForMatch(){
+function checkForMatch() {
+  const cards = document.querySelectorAll("img");
 
-	const cards = document.querySelectorAll("img");
+  if (selectedPokeball[0] === selectedPokeball[1]) {
+    cards[selectedPokeballId[0]].setAttribute(
+      "src",
+      "images/open-pokeball.png"
+    );
+    cards[selectedPokeballId[1]].setAttribute(
+      "src",
+      "images/open-pokeball.png"
+    );
+    cards[selectedPokeballId[0]].removeEventListener("click", flipCard);
+    cards[selectedPokeballId[1]].removeEventListener("click", flipCard);
+    openedPokeball.push(selectedPokeball);
 
-	if(selectedPokeball[0] === selectedPokeball[1]){
-		cards[selectedPokeballId[0]].setAttribute("src","images/open-pokeball.png");
-		cards[selectedPokeballId[1]].setAttribute("src","images/open-pokeball.png");
-		cards[selectedPokeballId[0]].removeEventListener("click",flipCard);
-		cards[selectedPokeballId[1]].removeEventListener("click",flipCard);
-		openedPokeball.push(selectedPokeball);
-		
-		renderScore();	
-	}
-	else 
-	{
-		cards[selectedPokeballId[0]].setAttribute("src","images/pokeball.png");
-		cards[selectedPokeballId[1]].setAttribute("src","images/pokeball.png");
-	}
-	selectedPokeball = [];
-	selectedPokeballId = [];
+    renderScore();
+  } else {
+    cards[selectedPokeballId[0]].setAttribute("src", "images/pokeball.png");
+    cards[selectedPokeballId[1]].setAttribute("src", "images/pokeball.png");
+  }
+  selectedPokeball = [];
+  selectedPokeballId = [];
 }
-
 
 function flipCard() {
   let id = this.getAttribute("data-id");
@@ -153,6 +193,6 @@ function flipCard() {
   count++;
   tries.textContent = count;
   if (selectedPokeball.length === 2) {
-  	setTimeout(checkForMatch,500);
+    setTimeout(checkForMatch, 500);
   }
-  }
+}
