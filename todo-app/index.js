d3733e213a7fd782943521f9f32145c07f970429
aswinr <aswinr2442002@gmail.com>
2022-09-03 11:46:44 +0530

    minor changes

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,10 @@
 let myTodos = [];
+let pendingTodos = [];
+let completedTodos = [];
 const inputEl = document.getElementById("input-el");
-const chkInputEl = document.getElementById("chk-btn-el");
+const chkBoxEl = document.getElementById("chk-box-el");
 const inptBtnEl = document.getElementById("inpt-btn-el");
-const AllEl = document.getElementById("all-el");
+const allEl = document.getElementById("all-el");
 const pndgEl = document.getElementById("pndg-el");
 const cmpltEl = document.getElementById("cmplt-el");
 const clrBtnEl = document.getElementById("clr-btn-el");
@@ -10,32 +12,76 @@ const moreEl = document.getElementById("more-el");
 const itemsEl = document.getElementById("items-el");
 
 const todosFromLocalStorage = JSON.parse(localStorage.getItem("myTodos"));
+const pendingTodosFromLocalStorage = JSON.parse(
+  localStorage.getItem("pendingTodos")
+);
+const completedTodosFromLocalStorage = JSON.parse(
+  localStorage.getItem("compledTodos")
+);
 
 if (todosFromLocalStorage) {
   myTodos = todosFromLocalStorage;
   render(myTodos);
 }
 
-function render(todos) {
-  let listItems = "";
+if (pendingTodosFromLocalStorage) {
+  pendingTodos = pendingTodosFromLocalStorage;
+}
 
-  for (let i = 0; i < todos.length; i++) {
-    listItems += `<li> <input id="chk-btn-el" type="checkbox"> ${todos[i]}  <span id="more-el">...</span></li>`;
+if (completedTodosFromLocalStorage) {
+  completedTodos = completedTodosFromLocalStorage;
+}
+
+function render(todos) {
+  if (!todos) {
+    itemsEl.innerHTML = "<li>No items to display!</li>";
+  } else {
+    let listItems = "";
+
+    for (let i = 0; i < todos.length; i++) {
+      listItems += `<li data-id="${i}"> <input id="chk-box-el" type="checkbox">
+       ${todos[i]}  <span id="more-el">...</span></li>`;
+    }
+    itemsEl.innerHTML = listItems;
   }
-  itemsEl.innerHTML = listItems;
 }
 
 inptBtnEl.addEventListener("click", function () {
   if (inputEl.value) {
     myTodos.push(inputEl.value);
+    pendingTodos.push(inputEl.value);
     localStorage.setItem("myTodos", JSON.stringify(myTodos));
+    localStorage.setItem("pendingTodos", JSON.stringify(pendingTodos));
     inputEl.value = "";
     render(myTodos);
   }
 });
 
+allEl.addEventListener("click", function () {
+  render(myTodos);
+});
+
+pndgEl.addEventListener("click", function () {
+  render(pendingTodos);
+});
+
+cmpltEl.addEventListener("click", function () {
+  render(completedTodos);
+});
+
+chkBoxEl.addEventListener("click", function () {
+  console.log(this);
+
+  const id = this.getAttribute("data-id");
+
+  completedTodos.push(pendingTodos[id]);
+  pendingTodos.splice(id,1);
+});
+
 clrBtnEl.addEventListener("click", function () {
   localStorage.clear();
   myTodos = [];
+  pendingTodos = [];
+  completedTodos = [];
   render(myTodos);
 });
